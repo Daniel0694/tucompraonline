cf56f59e41f28a07eb00387b1fae0db05ee1e30d
yprim <priscipo@gmail.com>
2017-05-31 21:29:41 -0600

    Se agrego la vista de Guest

---
+++ src/main/java/com/tucompraonline/controller/ProductoController.java
@@ -0,0 +1,8 @@
+package com.tucompraonline.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductoController {
+
+}
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,28 @@
 function home() {
 		document.getElementById('content').innerHTML = 'CARRITO'
 	}
+
+
+function showModal(name) {
+
+	var modal = "";
+
+	if (name == "ayuda")
+		modal = document.getElementById('modal-Ayuda');
+	else if (name == "acerca")
+		modal = document.getElementById('modal-Acercade');
+
+	modal.style.display = 'block';
+}
+
+function closeModal(name) {
+
+	var modal = "";
+
+	if (name == "ayuda")
+		modal = document.getElementById('modal-Ayuda');
+	else if (name == "acerca")
+		modal = document.getElementById('modal-Acercade');
+
+	modal.style.display = 'none';
+}
