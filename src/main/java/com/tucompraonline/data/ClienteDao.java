6f5d13044198c0a6e268b5a43dff102df00fcdd3
Daniel0694 <daniel_nuso@hotmail.com>
2017-05-31 21:03:01 -0600

    CRUD Cliente

--- src/main/java/com/tucompraonline/data/ClienteDao.java
+++ src/main/java/com/tucompraonline/data/ClienteDao.java
@@ -1,11 +1,18 @@
 package com.tucompraonline.data;
 
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tucompraonline.domain.Cliente;
 
 @Repository
 public class ClienteDao {
@@ -23,7 +30,69 @@ public class ClienteDao {
 		this.SJDBCEliminarCliente = new SimpleJdbcCall(dataSource).withProcedureName("deleteCliente");
 	}
 	
+	@Transactional
+	public Cliente insertarCliente (Cliente cliente){
+
+		
+		SqlParameterSource parameterSourceCliente = new MapSqlParameterSource()
+				.addValue("_cedula", cliente.getCedula())
+				.addValue("_nombre", cliente.getNombre())
+				.addValue("_correo_electronico", cliente.getCorreoElectronico())
+				.addValue("_telefono", cliente.getTelefono())
+				.addValue("_nombre_usuario", cliente.getNombreUsuario())
+				.addValue("_password", cliente.getPassword())
+				.addValue("_activo", cliente.isActivo())
+				.addValue("_pais", cliente.getPais())
+				.addValue("_estado", cliente.getEstado())
+				.addValue("_ciudad", cliente.getCedula())
+				.addValue("_cod_postal", cliente.getCodigoPostal())
+				.addValue("_direccion", cliente.getDireccion());
+		Map<String, Object> outParameters = SJDBCAgregarCliente.execute(parameterSourceCliente);
+		
+		cliente.setIdUsuario(Integer.parseInt(outParameters.get("_id_usuario").toString()));
+		
+		
+		return cliente;
+	}
 	
+	@Transactional
+	public boolean actualizaCliente (Cliente cliente){
+		
+		try {
+			SqlParameterSource parameterSourceCliente = new MapSqlParameterSource()
+					.addValue("id_usuario", cliente.getIdUsuario())
+					.addValue("_cedula", cliente.getCedula())
+					.addValue("_nombre", cliente.getNombre())
+					.addValue("_correo_electronico", cliente.getCorreoElectronico())
+					.addValue("_telefono", cliente.getTelefono())
+					.addValue("_nombre_usuario", cliente.getNombreUsuario())
+					.addValue("_password", cliente.getPassword())
+					.addValue("_activo", cliente.isActivo())
+					.addValue("_pais", cliente.getPais())
+					.addValue("_estado", cliente.getEstado())
+					.addValue("_ciudad", cliente.getCedula())
+					.addValue("_cod_postal", cliente.getCodigoPostal())
+					.addValue("_direccion", cliente.getDireccion());
+			SJDBCAgregarCliente.execute(parameterSourceCliente);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+	
+	@Transactional
+	public boolean eliminaCliente( int idUsuario){
+		try {
+			SqlParameterSource procedimientoEliminarEmpleado = new MapSqlParameterSource()
+					.addValue("_id_usuario",idUsuario);
+			SJDBCEliminarCliente.execute(procedimientoEliminarEmpleado);
+			return true;
+		} catch (Error e) {
+			return false;
+		}
+	}
+	
+
 	
 	
 }
