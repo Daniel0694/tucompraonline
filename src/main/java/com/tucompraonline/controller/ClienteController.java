3a4edd0337dd25c48ffed50da50baf87ca42014c
yprim <priscipo@gmail.com>
2017-06-02 01:10:54 -0600

    vista del cliente lista solo falta  el carrito volatil

--- src/main/java/com/tucompraonline/controller/ClienteController.java
+++ src/main/java/com/tucompraonline/controller/ClienteController.java
@@ -1,8 +1,29 @@
 package com.tucompraonline.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ClienteController {
+	
+	
+
+	@RequestMapping(value = "/actualizarCliente", method = RequestMethod.GET)
+	public String showActualizarCliente() {
+		
+		return "actualizarCliente ";
+			
+		
+	}
+	
+	
+	@RequestMapping(value = "/actualizarCliente/actualizar", method = RequestMethod.GET)
+	public String actualizarCliente() {
+		
+		return "success";
+			
+		
+	}
 
 }
