87f68517f8674c37cdfe394bcbe489be04128b5d
Daniel0694 <daniel_nuso@hotmail.com>
2017-06-02 08:08:37 -0600

    controllers para cliente (falta implementar completamente)

--- src/main/java/com/tucompraonline/controller/ClienteController.java
+++ src/main/java/com/tucompraonline/controller/ClienteController.java
@@ -1,28 +1,46 @@
 package com.tucompraonline.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.tucompraonline.business.ClienteService;
+import com.tucompraonline.domain.Cliente;
+import com.tucompraonline.domain.Usuario;
+
+
 @Controller
 public class ClienteController {
 	
-	
+	@Autowired
+	ClienteService clienteService;
 
+	
+	
+	
 	@RequestMapping(value = "/actualizarCliente", method = RequestMethod.GET)
-	public String showActualizarCliente() {
+	public String showActualizarCliente(Cliente cliente) {
 		
-		return "actualizarCliente ";
+		return "actualizarCliente";
 			
 		
 	}
 	
 	
-	@RequestMapping(value = "/actualizarCliente/actualizar", method = RequestMethod.GET)
-	public String actualizarCliente() {
+	@RequestMapping(value = "/actualizarCliente/actualizar", method = RequestMethod.POST)
+	public String actualizarCliente(Cliente cliente) {
 		
-		return "success";
-			
+		
+			boolean actualizado = clienteService.actualizaCliente(cliente);
+			if (actualizado){
+				return "paginaPrincipalCliente";
+			}
+			else{
+				return "error";
+			}
+				
 		
 	}
 
